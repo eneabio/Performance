d074afdae706db0a5791e99ed09c95da4537a8bb
Enea Bionda <eneabio1974@gmail.com>
2013-03-04 15:55:17 +0100

    Added Windows support.

--- performance.h
+++ performance.h
@@ -5,13 +5,25 @@
 
 #ifndef PERFORMANCE_H__
 #define PERFORMANCE_H__
+#endif
 
-#include <stdint.h>
+#ifdef _WIN32
+	//#include <time.h>
+	//typedef clock_t  Clock;
+	//#include <stdint.h>
+	#include <windows.h>
+	#include <stdio.h>
+	typedef __int64 Clock;
+
+//Apple systems
+#elif defined (__APPLE__)
+	#include <stdint.h>
+	typedef uint64_t Clock;
+#endif
 
 namespace performance {
-	uint64_t   Start          ();
-	uint64_t   Stop           ();
-	double   ExecutionTime  (uint64_t start, uint64_t stop);
+	Clock   Start          ();
+	Clock   Stop           ();
+	double   ExecutionTime  (Clock start, Clock stop);
 }
 
-#endif
---
+++ performance_win.cpp
@@ -0,0 +1,60 @@
+// Copyright (C) 2012 Reply S.p.A. and/or its subsidiary(-ies). All rights reserved.
+// Released under the the GNU Lesser General Public License version 2.1. In addition, as a special exception, Reply S.p.A. gives you certain additional rights.
+// Alternatively, this file may be used in accordance with the terms and conditions contained in a signed written agreement between you and Reply S.p.A., and/or its subsidiary(-ies).
+// See Licence.txt
+
+#include "performance.h"
+
+namespace performance {
+
+	//double subtractTimes( Clock startTime, Clock endTime )
+	//{	
+	//	//to do: verify it...
+	//	double timediff = ((double) (endTime - startTime)/CLOCKS_PER_SEC );  //CLOCKS_PER_SEC=1000 , return seconds
+	//	return timediff;
+	//}
+
+
+	Clock   Start() {
+		//return clock();
+		LARGE_INTEGER li;
+	double PCFreq = 0.0;
+	__int64 CounterStart = 0;
+    if(!QueryPerformanceFrequency(&li))
+	printf("QueryPerformanceFrequency failed!\n");
+
+    PCFreq = double(li.QuadPart);  //out is in seconds...
+
+    QueryPerformanceCounter(&li);
+    CounterStart = li.QuadPart;
+	return CounterStart;
+	}
+	Clock   Stop() {
+		//return clock();
+		LARGE_INTEGER li;
+	double PCFreq = 0.0;
+	__int64 CounterStop = 0;
+    if(!QueryPerformanceFrequency(&li))
+	printf("QueryPerformanceFrequency failed!\n");
+
+    PCFreq = double(li.QuadPart);  //out is in seconds...
+
+    QueryPerformanceCounter(&li);
+    CounterStop = li.QuadPart;
+	return CounterStop;
+	}
+
+	double   ExecutionTime  (Clock start, Clock stop) {
+		//return subtractTimes(start, stop);
+		 LARGE_INTEGER li;
+	double PCFreq = 0.0;
+	 if(!QueryPerformanceFrequency(&li))
+	printf("QueryPerformanceFrequency failed!\n");
+
+    PCFreq = double(li.QuadPart);   //out is in seconds...
+	//QueryPerformanceCounter(&li);
+    return double(stop-start)/PCFreq;
+
+	}
+
+}
